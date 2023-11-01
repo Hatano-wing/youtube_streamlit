89ab0853da7be4ce072fcb5075f17def48f5e91d
Ri <Ri>
2023-11-01 21:01:57 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import time
+
+#タイトル記入
+st.title('Streamlit超入門')
+
+#コメント追加
+st.write('プログレスバーの表示')
+'start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!'
